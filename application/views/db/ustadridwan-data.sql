fa727d990beed2ea5cb7dd7ea78d5dc9032299a8
Muhammad Yusuf Bachtiar <yusuf@artikulpi.com>
2014-10-18 09:42:28 +0700

    Update ustadridwan-data.sql

--- application/views/db/ustadridwan-data.sql
+++ application/views/db/ustadridwan-data.sql
@@ -18,9 +18,9 @@ SET time_zone = "+00:00";
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_name`, `user_password`, `user_full_name`, `user_description`, `user_freeze`, `user_role`) VALUES
-(1, 'admin', '10d0b55e0ce96e1ad711adaac266c9200cbc27e4', 'Admin', 'Administrator', 0, '1'),
-(2, 'demo', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'Demo', 'Demo', 0, '2');
+INSERT INTO `user` (`user_id`, `user_name`, `user_password`, `user_full_name`, `user_description`, `user_active`, `user_role`) VALUES
+(1, 'admin', '10d0b55e0ce96e1ad711adaac266c9200cbc27e4', 'Admin', 'Administrator', 1, '1'),
+(2, 'demo', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'Demo', 'Demo', 1, '2');
 
 INSERT INTO  `profile` (
 `profile_id` ,
